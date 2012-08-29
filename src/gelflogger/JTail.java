55d2ede51ae5adb320ad9f1dbd91ae5b45c6bd11
Krisa <krisachai@gmail.com>
2012-08-29 21:58:54 +0700

    Update JTail
    code change to avoid stack overflow while checking whether file is exist
    or not

--- src/gelflogger/JTail.java
+++ src/gelflogger/JTail.java
@@ -10,47 +10,83 @@ import java.lang.*;
 import java.util.*;
 
 class JTail {
-
-     MessageListener msl;
+    boolean isNeedRun = true;
+    MessageListener msl;
 
     public void setMessageListener(MessageListener msl) {
-        
+
         this.msl = msl;
-        
+
     }
+
     public JTail(MessageListener msl) {
-        
+
         this.msl = msl;
-        
+
     }
-    public  void tail(String file) throws InterruptedException, IOException {
+
+    public void tail(String file) throws InterruptedException, IOException {
         
+        while (isNeedRun) {
+            waitUntilFileExist(file);
+            try {
+                readFile(file);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void readFile(String file) throws InterruptedException, IOException {
         File f = new File(file);
-        
-        BufferedReader br = new BufferedReader(new FileReader(file.trim()));
-        
-        br.skip(f.length());
-        String line = null;
-        
-        while (true) {
-            
-            line = br.readLine();
-            
-            if (line == null) {
 
+        FileReader reader = new FileReader(file.trim());
+        try {
+            BufferedReader br = new BufferedReader(reader);
+            br.skip(f.length());
+            String line;
 
-                Thread.sleep(1000);
-                
-            } else {
-                if (msl != null) {
-                  
-                    msl.onMessage(line);
+            while (f.exists()) {
+
+                f = new File(file);
+                line = br.readLine();
+
+                if (line == null) {
+
+                    Thread.sleep(1000);
+
+                } else {
+                    if (msl != null) {
+                        //Send line to attached interface
+                        msl.onMessage(line);
+                    }
                 }
             }
-            
+        } finally {
+            try {
+                reader.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        
-        
-        
-    } //end main
-} //end class jtail
+    }
+
+    private void waitUntilFileExist(String file) throws InterruptedException {
+        File f = new File(file);
+        while (!f.exists()) {
+            System.out.println("File doesn't exists");
+            Thread.sleep(10000);
+        }
+        System.out.println("File does exists");
+    }
+
+    public static void main(String[] args) throws Exception {
+        /*
+         * JTail testTail = new JTail(new MessageListener() {
+         *
+         * @Override public void onMessage(String msg) {
+         * System.out.println(msg); } }); testTail.tail("F:\\file.log");
+         */
+        System.out.println(new Date(System.currentTimeMillis()));
+    }
+}
